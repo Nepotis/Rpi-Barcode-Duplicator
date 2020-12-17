085b663508e6b4c42a76456e7457c3dc37a87137
Nepotis <9596528+Nepotis@users.noreply.github.com>
2020-12-17 14:18:57 -0500

    Update printbarcode.sh

--- printbarcode.sh
+++ printbarcode.sh
@@ -1,7 +1,7 @@
 while :
 do
   read bc 
-  zpl1="^XA~SD30^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR4,4^MD15^LRN^CI0"
+  zpl1="^XA~SD15^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR4,4^MD15^LRN^CI0"
   zpl2="^MMT^PW600^LL0150^LS0^BY4,3,81^FT76,96^BCN,,Y,N^FD"
   zpl3="^FS^XZ"
   fullzpl=$zpl1$zpl2$bc$zpl3 #This inserts the variable bc in two places in the script, once for the custom interpretation line and once for the Code 128 barcode.  See the README.md file for details.
