bc5f1dc5bb00c212649f67d8019956143a4c2859
Nepotis <9596528+Nepotis@users.noreply.github.com>
2020-12-17 14:08:50 -0500

    Update printbarcode.sh

--- printbarcode.sh
+++ printbarcode.sh
@@ -1,11 +1,11 @@
 while :
 do
   read bc 
-  zpl1="^XA~SD30^LT18^FO230,4^A0N,50,55^FD"
-  zpl2="^FS^BY2,2,50^FO220,55^BCN,50,N,N,N,N^FD"
-  zpl3="FS^XZ"
+  zpl1="~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR4,4^MD15^LRN^CI0"
+  zpl2="^MMT^PW600^LL0150^LS0^BY4,3,81^FT76,96^BCN,,Y,N^FD"
+  zpl3="^FS^XZ"
   fullzpl=$zpl1$bc$zpl2$bc$zpl3 #This inserts the variable bc in two places in the script, once for the custom interpretation line and once for the Code 128 barcode.  See the README.md file for details.
   echo $fullzpl
-  echo $fullzpl > /tmp/bc.txt
+  #echo $fullzpl > /tmp/bc.txt
   lp -dZebra -o raw /tmp/bc.txt
 done
